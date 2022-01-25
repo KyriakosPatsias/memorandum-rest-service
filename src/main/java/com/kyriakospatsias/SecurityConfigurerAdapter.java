768b541dddca5ecef33e13efb3b5cc2b6416d56f
Kyriakos Patsias <kpatsias@gmail.com>
2022-01-25 15:13:04 -0800

    Rename security Configurer Adapter

--- src/main/java/com/kyriakospatsias/SecurityConfigurerAdapter.java
+++ src/main/java/com/kyriakospatsias/SecurityConfigurerAdapter.java
@@ -1,16 +1,13 @@
 package com.kyriakospatsias;
 
-import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
-
 @Configuration
 @EnableWebSecurity
-public class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+public class SecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
