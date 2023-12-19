1a5e775cc4ca1199f65bda3fdccef8be2ed370b4
Kyriakos Patsias <kpatsias@gmail.com>
2023-12-18 16:51:15 -0800

    Remove autowired annotation. Update imports.

--- src/main/java/com/kyriakospatsias/NoteController.java
+++ src/main/java/com/kyriakospatsias/NoteController.java
@@ -2,8 +2,14 @@ package com.kyriakospatsias;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -14,8 +20,6 @@ import java.util.Optional;
 public class NoteController {
 
     Logger logger = LoggerFactory.getLogger(NoteController.class);
-
-    @Autowired
     NoteRepository noteRepository;
 
     @GetMapping(value = "/note")
