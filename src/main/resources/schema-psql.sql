6bea941055d6e81d12b8b83db5d76fb08a0b80d6
Kyriakos Patsias <kpatsias@gmail.com>
2026-06-05 11:35:55 -0700

    Add users table devinition

--- src/main/resources/schema-psql.sql
+++ src/main/resources/schema-psql.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS note  (
   content VARCHAR NULL,
   category VARCHAR(50) NULL,
   is_favorite BOOLEAN DEFAULT FALSE,
+  userId SERIAL REFERENCES users(userId),
   created_on TIMESTAMP,
   updated_on TIMESTAMP
 );
@@ -13,3 +14,14 @@ CREATE TABLE IF NOT EXISTS category  (
     name VARCHAR(50) NULL,
     color VARCHAR(20) NULL
 );
+
+
+CREATE TABLE IF NOT EXISTS users  (
+    userId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    timezone VARCHAR(20) NULL,
+    created_on TIMESTAMP,
+    updated_on TIMESTAMP
+    );
